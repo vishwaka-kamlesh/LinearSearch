3905d2832d66fe6a90f090c239f2921a85a82b7e
vishwaka-kamlesh <137268987+vishwaka-kamlesh@users.noreply.github.com>
2023-07-17 13:01:27 +0700

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,25 +1,24 @@
-// Linear search in an one-dimensional array
+// Linear search in a one-dimensional array
 
 class LinearSearch {
-    
-    static int linearSearch(int[] nums, int target)
-    {
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]==target)
+    // Method for linear search
+    static int linearSearch(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target)
                 return i;
         }
         return -1;
     }
+    
     public static void main(String[] args) {
-        
-        int[] nums = { 1,2,3,4,8,9,6,7 };
+        int[] nums = { 1, 2, 3, 4, 8, 9, 6, 7 };
         int target = 7;
         int result = linearSearch(nums, target);
-        if(result!=-1)
-            System.out.println("Element found at position "+result);
+
+        // Print the result
+        if (result != -1)
+            System.out.println("Element found at position " + result);
         else
             System.out.println("Element not found");
-        
     }
 }
